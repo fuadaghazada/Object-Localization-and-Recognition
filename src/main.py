f70dc2d90a90668268cb7121b57f6e2ca40e51ab
Fuad Aghazada <fuad.aghazada98@gmail.com>
2019-05-28 15:29:36 +0300

    Saving SVM models

--- src/main.py
+++ src/main.py
@@ -1,3 +1,5 @@
+import pickle
+
 import numpy as np
 import cv2
 
@@ -8,26 +10,42 @@ from evaluation import read_test_data, evaluate1, evaluate2
 
 from training_svm import train_SVM
 
+# Don't train: load the saved models
+TRAIN_FLAG = False
+
+class_labels = np.asarray(['n01615121', 'n02099601', 'n02123159', 'n02129604', 'n02317335',
+						   'n02391049', 'n02410509', 'n02422699', 'n02481823', 'n02504458'])
+
 # Creating ResNet50 model
 model = resnet50(pretrained=True)
 
-# Loading the train set (feature vectors and feature label)
-train_features = load_train_dataset(model)
+if TRAIN_FLAG is True:
+	# Loading the train set (feature vectors and feature label)
+	train_features = load_train_dataset(model)
 
-# Feature vectors and label for Train set
-train_feature_vectors = [feature['feature_vec'] for feature in train_features]
-train_feature_labels = [feature['label'] for feature in train_features]
-class_labels = np.unique(train_feature_labels)
+	# Feature vectors and label for Train set
+	train_feature_vectors = [feature['feature_vec'] for feature in train_features]
+	train_feature_labels = [feature['label'] for feature in train_features]
 
-# -- TRAINING --
+	# -- TRAINING --
 
-# One vs all training
-svm_models = train_SVM(train_feature_vectors, train_feature_labels, class_labels)
+	# One vs all training
+	svm_models = train_SVM(train_feature_vectors, train_feature_labels, class_labels)
+
+	# Saving the models
+	for i, svm_model in enumerate(svm_models):
+		pickle.dump(svm_model, open('../model/svm_model_{}.obj'.format(i), 'wb'))
 
 # Edge detection object ('Edge box' method)
 edge_detection = cv2.ximgproc.createStructuredEdgeDetection('../model/model.yml')
 
 # -- TESTING --
+
+# Loading the models
+svm_models = []
+for i in range(10):
+	svm_model = pickle.load(open('../model/svm_model_{}.obj'.format(i), 'rb'))
+	svm_models.append(svm_model)
 
 # Testing results
 test_predictions, box_boundaries = test(edge_detection, model, svm_models)
